5cfa4546d7b299650a892e40302db40ad0648a1d
assoouu <lloe1802@gmail.com>
2024-09-05 23:45:30 +0900

    Delete hotel-booking directory

--- hotel-booking/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const HotelBooking = artifacts.require("HotelBooking");
-
-module.exports = async function(deployer) {
-    await deployer.deploy(HotelBooking);
-    const instance = await HotelBooking.deployed();
-    await instance.addRoom("Deluxe Room", web3.utils.toWei("0.5", "ether"));
-    await instance.addRoom("Standard Room", web3.utils.toWei("0.3", "ether"));
-};
